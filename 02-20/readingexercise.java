d439164bbf09d1dfdbfc1c0f0f4d5c98c513aa4b
Joshua Tibatemwa <tibatemw@cs.grinnell.edu>
2017-02-20 14:54:44 -0600

    This is done

---
+++ 02-20/readingexercise.java
@@ -0,0 +1,23 @@
+public class Node{
+
+    public int value;
+    private Node next;
+
+    public Node(int value, Node next){
+        this.value=value;
+        this.next=next;
+        
+
+    }}
+
+public class Linkedlist{
+
+    private Node first;
+
+    public Linkedlist(){
+
+        this.first=null;
+    }
+
+
+}
